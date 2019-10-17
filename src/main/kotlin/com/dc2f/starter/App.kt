aa9cf53bfbcc94635ecdc0ef644cd609ddf73f23
Herbert Poul <herbert@codeux.design>
2019-10-17 11:52:15 +0200

    organize imports.

--- src/main/kotlin/com/dc2f/starter/App.kt
+++ src/main/kotlin/com/dc2f/starter/App.kt
@@ -2,9 +2,8 @@ package com.dc2f.starter
 
 import com.dc2f.common.*
 import com.dc2f.common.contentdef.BaseWebsite
-import com.dc2f.common.theme.*
-import com.dc2f.render.*
-import com.dc2f.util.Dc2fConfig
+import com.dc2f.common.theme.BaseTheme
+import com.dc2f.render.ThemeConfig
 
 class WebsiteTheme : BaseTheme() {
     override fun configure(config: ThemeConfig) {
